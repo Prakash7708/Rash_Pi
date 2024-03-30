8b11e9e329711af93afa33e68427322f3735e6eb
Prakash <103762233+Prakash7708@users.noreply.github.com>
2024-03-30 18:17:34 +0530

    pin 17

--- Controllers/Test.js
+++ Controllers/Test.js
@@ -1,33 +1,31 @@
-
-
 const gpio = require('rpi-gpio');
 
 exports.testReq = async function (req, res) {
     try {
-        // Set up GPIO pin 1 as an output for powering the LED
-        gpio.setup(1, gpio.DIR_OUT, async function(err) {
+        // Set up GPIO pin 17 as an output for powering the LED
+        gpio.setup(17, gpio.DIR_OUT, async function(err) {
             if (err) {
                 throw err;
             }
 
             // Turn the pin on to supply power to the LED
-            gpio.write(1, true, function(err) {
+            gpio.write(17, true, function(err) {
                 if (err) {
                     throw err;
                 }
-                console.log('Pin 1 (3.3V) is supplying power to the LED');
+                console.log('Pin 17 is supplying power to the LED');
                 res.status(200).json({ status: "success" });
             });
 
-            // Wait for 1 second (you can adjust this time as needed)
+            // Wait for 3 seconds (you can adjust this time as needed)
             await new Promise(resolve => setTimeout(resolve, 3000));
 
             // Turn the pin off to stop supplying power to the LED
-            gpio.write(1, false, function(err) {
+            gpio.write(17, false, function(err) {
                 if (err) {
                     throw err;
                 }
-                console.log('Pin 1 (3.3V) is turned off');
+                console.log('Pin 17 is turned off');
             });
         });
     } catch (error) {
