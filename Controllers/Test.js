f28fa6d028b6f198cb9713daeff2080a8ac72432
Prakash <103762233+Prakash7708@users.noreply.github.com>
2024-03-30 19:16:43 +0530

    on off pakage

--- Controllers/Test.js
+++ Controllers/Test.js
@@ -1,33 +1,19 @@
-const gpio = require('rpi-gpio');
+const { Gpio } = require('onoff'); // Include onoff to interact with the GPIO
+const LED = new Gpio(17, 'out'); // Use GPIO pin 17 and specify that it is output
 
 exports.testReq = async function (req, res) {
     try {
-        // Set up GPIO pin 17 as an output for powering the LED
-        gpio.setup(17, gpio.DIR_OUT, async function(err) {
-            if (err) {
-                throw err;
-            }
+        // Turn the pin on to supply power to the LED
+        LED.writeSync(1); // Set pin state to 1 (turn LED on)
+        console.log('Pin 17 is supplying power to the LED');
+        res.status(200).json({ status: "success" });
 
-            // Turn the pin on to supply power to the LED
-            gpio.write(17, true, function(err) {
-                if (err) {
-                    throw err;
-                }
-                console.log('Pin 17 is supplying power to the LED');
-                res.status(200).json({ status: "success" });
-            });
+        // Wait for 3 seconds (you can adjust this time as needed)
+        await new Promise(resolve => setTimeout(resolve, 3000));
 
-            // Wait for 3 seconds (you can adjust this time as needed)
-            await new Promise(resolve => setTimeout(resolve, 3000));
-
-            // Turn the pin off to stop supplying power to the LED
-            gpio.write(17, false, function(err) {
-                if (err) {
-                    throw err;
-                }
-                console.log('Pin 17 is turned off');
-            });
-        });
+        // Turn the pin off to stop supplying power to the LED
+        LED.writeSync(0); // Set pin state to 0 (turn LED off)
+        console.log('Pin 17 is turned off');
     } catch (error) {
         console.log(error);
         res.status(500).json({ status: "failure", message: "Failed to control GPIO pin" });
