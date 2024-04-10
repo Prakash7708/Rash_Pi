48c4cbd648e303c61076d94e1b42e5cf222ba464
Prakash <103762233+Prakash7708@users.noreply.github.com>
2024-04-10 20:21:21 +0530

    npm cam

--- Controllers/Camera.js
+++ Controllers/Camera.js
@@ -1,7 +1,6 @@
-const { exec } = require('child_process');
+const raspividStream = require('raspberry-live-stream');
 
 // Endpoint to stream live video from camera
-
 exports.liveCam = async function (req, res) {
     // Set content type to video/mp4
     res.writeHead(200, {
@@ -10,20 +9,59 @@ exports.liveCam = async function (req, res) {
         'Transfer-Encoding': 'chunked'
     });
 
-    // Execute raspivid command to stream video
-    const raspividProcess = exec('raspivid -t 0 -hf -vf -fps 30 -b 1000000 -o -');
+    // Set up Raspberry Pi camera stream
+    const stream = raspividStream();
 
     // Pipe video stream to HTTP response
-    raspividProcess.stdout.pipe(res);
-
-    // Handle process exit
-    raspividProcess.on('exit', (code) => {
-        console.log(`raspivid process exited with code ${code}`);
-    });
+    stream.pipe(res);
 
     // Handle client disconnect
     req.on('close', () => {
         console.log('Client disconnected');
-        raspividProcess.kill(); // Stop streaming when client disconnects
+        stream.end(); // End streaming when client disconnects
     });
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const { exec } = require('child_process');
+
+// // Endpoint to stream live video from camera
+
+// exports.liveCam = async function (req, res) {
+//     // Set content type to video/mp4
+//     res.writeHead(200, {
+//         'Content-Type': 'video/mp4',
+//         'Connection': 'keep-alive',
+//         'Transfer-Encoding': 'chunked'
+//     });
+
+//     // Execute raspivid command to stream video
+//     const raspividProcess = exec('raspivid -t 0 -hf -vf -fps 30 -b 1000000 -o -');
+
+//     // Pipe video stream to HTTP response
+//     raspividProcess.stdout.pipe(res);
+
+//     // Handle process exit
+//     raspividProcess.on('exit', (code) => {
+//         console.log(`raspivid process exited with code ${code}`);
+//     });
+
+//     // Handle client disconnect
+//     req.on('close', () => {
+//         console.log('Client disconnected');
+//         raspividProcess.kill(); // Stop streaming when client disconnects
+//     });
+// };
--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(process.env.PORT || 5001,()=>{
-console.log("api Running in 5000")
+console.log("api Running in 5001")
 });
 
    
