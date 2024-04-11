95d8edbddce33b86ce38c1260cf596a247ef4a26
Prakash <103762233+Prakash7708@users.noreply.github.com>
2024-04-12 01:23:57 +0530

    new succes methode

--- Controllers/Camera.js
+++ Controllers/Camera.js
@@ -1,65 +1,90 @@
-// Import necessary modules
-var NodeWebcam = require("node-webcam");
-var fs = require("fs");
-var path = require("path");
 
-// Define the folder path
-var folderPath = '/var/app/Rash_Pi/photos';
 
-// Create the folder if it doesn't exist
-if (!fs.existsSync(folderPath)) {
-    fs.mkdirSync(folderPath);
-}
 
-// Default options for capturing image
-var opts = {
-    // Picture related
-    width: 1280,
-    height: 720,
-    quality: 100,
-    // Save shots in memory
-    saveShots: true,
-    // Output type
-    output: "jpeg",
-    // Which camera to use, false for default device
-    device: false,
-    // Logging
-    verbose: false,
-    // File path to save captured image
-    callbackReturn: "location" // Specify that you want the file location as callback return
-};
-
-// Create webcam instance
-var Webcam = NodeWebcam.create(opts);
 
 // Endpoint to stream live video from camera
 exports.liveCam = async function (req, res) {
-    // Set content type to image/jpeg
-    res.writeHead(200, { 'Content-Type': 'image/jpeg' });
-
-    // Print the folder path for debugging
-    console.log("Folder Path:", folderPath);
-     console.log(Webcam)
-    // Capture an image from webcam
-    Webcam.capture(path.join(folderPath, "./test_picture"), async function (err, data) {
-        if (err) {
-            console.error(err);
-            res.end();
-        } else {
-            // Read the captured image
-            fs.readFile(data, function (err, imageData) {
-                if (err) {
-                    console.error(err);
-                    res.end();
-                } else {
-                    // Send the image as response
-                    console.log(imageData)
-                    res.end(imageData);
-                }
-            });
-        }
-    });
+    try {
+        
+    } catch (error) {
+        
+    }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Import necessary modules
+// var NodeWebcam = require("node-webcam");
+// var fs = require("fs");
+// var path = require("path");
+
+// // Define the folder path
+// var folderPath = '/var/app/Rash_Pi/photos';
+
+// // Create the folder if it doesn't exist
+// if (!fs.existsSync(folderPath)) {
+//     fs.mkdirSync(folderPath);
+// }
+
+// // Default options for capturing image
+// var opts = {
+//     // Picture related
+//     width: 1280,
+//     height: 720,
+//     quality: 100,
+//     // Save shots in memory
+//     saveShots: true,
+//     // Output type
+//     output: "jpeg",
+//     // Which camera to use, false for default device
+//     device: false,
+//     // Logging
+//     verbose: false,
+//     // File path to save captured image
+//     callbackReturn: "location" // Specify that you want the file location as callback return
+// };
+
+// // Create webcam instance
+// var Webcam = NodeWebcam.create(opts);
+
+// // Endpoint to stream live video from camera
+// exports.liveCam = async function (req, res) {
+//     // Set content type to image/jpeg
+//     res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+
+//     // Print the folder path for debugging
+//     console.log("Folder Path:", folderPath);
+//      console.log(Webcam)
+//     // Capture an image from webcam
+//     Webcam.capture(path.join(folderPath, "./test_picture"), async function (err, data) {
+//         if (err) {
+//             console.error(err);
+//             res.end();
+//         } else {
+//             // Read the captured image
+//             fs.readFile(data, function (err, imageData) {
+//                 if (err) {
+//                     console.error(err);
+//                     res.end();
+//                 } else {
+//                     // Send the image as response
+//                     console.log(imageData)
+//                     res.end(imageData);
+//                 }
+//             });
+//         }
+//     });
+// };
 
 
 
