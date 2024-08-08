0ea3db4d5319d9b16114e35c07552774b3d23f35
Prakash <103762233+Prakash7708@users.noreply.github.com>
2024-08-08 22:14:32 +0530

    dd

--- Controllers/Test.js
+++ Controllers/Test.js
@@ -26,50 +26,50 @@
 
 
 
-// // Define GPIO pins for motor control
-//const { Gpio } = require('onoff');
-// const in1 = new Gpio(22, 'out'); // Use GPIO 22 for IN1 on L298N
-// const in2 = new Gpio(27, 'out'); // Use GPIO 27 for IN2 on L298N
+// Define GPIO pins for motor control
+const { Gpio } = require('onoff');
+const in1 = new Gpio(22, 'out'); // Use GPIO 22 for IN1 on L298N
+const in2 = new Gpio(27, 'out'); // Use GPIO 27 for IN2 on L298N
 
-// // Variable to track motor state
-// let isMotorOn = false;
+// Variable to track motor state
+let isMotorOn = false;
 
-// // Function to drive motor forward
-// function motorForward() {
-//     in1.writeSync(1);
-//     in2.writeSync(0);
-// }
+// Function to drive motor forward
+function motorForward() {
+    in1.writeSync(1);
+    in2.writeSync(0);
+}
 
-// // Function to stop the motor
-// function motorStop() {
-//     in1.writeSync(0);
-//     in2.writeSync(0);
-// }
+// Function to stop the motor
+function motorStop() {
+    in1.writeSync(0);
+    in2.writeSync(0);
+}
 
-// exports.testReq = async function (req, res) {
-//     try {
-//         // Toggle motor state
-//         isMotorOn = !isMotorOn;
+exports.testReq = async function (req, res) {
+    try {
+        // Toggle motor state
+        isMotorOn = !isMotorOn;
 
-//         // Set the motor state based on the isMotorOn value
-//         if (isMotorOn) {
-//             motorForward();
-//         } else {
-//             motorStop();
-//         }
+        // Set the motor state based on the isMotorOn value
+        if (isMotorOn) {
+            motorForward();
+        } else {
+            motorStop();
+        }
 
-//         res.status(200).json({ status: "success", isMotorOn });
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).json({ status: "failure", message: "Failed to control GPIO pin" });
-//     }
-// };
+        res.status(200).json({ status: "success", isMotorOn });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ status: "failure", message: "Failed to control GPIO pin" });
+    }
+};
 
-// // Cleanup GPIO on exit
-// process.on('SIGINT', () => {
-//     in1.unexport();
-//     in2.unexport();
-//     process.exit();
-// });
+// Cleanup GPIO on exit
+process.on('SIGINT', () => {
+    in1.unexport();
+    in2.unexport();
+    process.exit();
+});
 
 
