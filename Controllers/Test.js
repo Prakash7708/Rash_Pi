d1c2cd4bccb4282ea3500c56b8220c86bba8c4d0
Prakash <103762233+Prakash7708@users.noreply.github.com>
2024-10-27 23:45:42 +0530

    2 motors

--- Controllers/Test.js
+++ Controllers/Test.js
@@ -25,47 +25,72 @@
 // };
 
 
-
-// Define GPIO pins for motor control
 const { Gpio } = require('onoff');
-const in1 = new Gpio(22, 'out'); // Use GPIO 22 for IN1 on L298N
-const in2 = new Gpio(27, 'out'); // Use GPIO 27 for IN2 on L298N
 
-// Variable to track motor state
-let isMotorOn = false;
+// Define GPIO pins for Motor 1
+const in1 = new Gpio(22, 'out'); // IN1 on L298N for Motor 1
+const in2 = new Gpio(27, 'out'); // IN2 on L298N for Motor 1
 
-// Function to drive motor forward
-function motorForward() {
-    console.log('forward')
+// Define GPIO pins for Motor 2
+const in3 = new Gpio(17, 'out'); // IN3 for Motor 2
+const in4 = new Gpio(18, 'out'); // IN4 for Motor 2
+
+// Variables to track motor states
+let isMotor1On = false;
+let isMotor2On = false;
+
+// Function to drive Motor 1 forward
+function motor1Forward() {
+    console.log('Motor 1 forward');
     in1.writeSync(1);
     in2.writeSync(0);
 }
 
-// Function to stop the motor
-function motorStop() {
+// Function to stop Motor 1
+function motor1Stop() {
     in1.writeSync(0);
     in2.writeSync(0);
 }
 
+// Function to drive Motor 2 forward
+function motor2Forward() {
+    console.log('Motor 2 forward');
+    in3.writeSync(1);
+    in4.writeSync(0);
+}
+
+// Function to stop Motor 2
+function motor2Stop() {
+    in3.writeSync(0);
+    in4.writeSync(0);
+}
+
 exports.testReq = async function (req, res) {
     try {
-        // Toggle motor state
-        isMotorOn = !isMotorOn;
+        // Toggle motor states for Motor 1 and Motor 2
+        isMotor1On = !isMotor1On;
+        isMotor2On = !isMotor2On;
 
-        // Set the motor state based on the isMotorOn value
-        if (isMotorOn) {
-            motorForward();
-         
-            console.log("motor on")
+        if (isMotor1On) {
+            motor1Forward();
+            console.log("Motor 1 on");
         } else {
-            motorStop();
-            console.log("motor off")
+            motor1Stop();
+            console.log("Motor 1 off");
         }
 
-        res.status(200).json({ status: "success", isMotorOn });
+        if (isMotor2On) {
+            motor2Forward();
+            console.log("Motor 2 on");
+        } else {
+            motor2Stop();
+            console.log("Motor 2 off");
+        }
+
+        res.status(200).json({ status: "success", isMotor1On, isMotor2On });
     } catch (error) {
         console.log(error);
-        res.status(500).json({ status: "failure", message: "Failed to control GPIO pin" });
+        res.status(500).json({ status: "failure", message: "Failed to control GPIO pins" });
     }
 };
 
@@ -73,8 +98,63 @@ exports.testReq = async function (req, res) {
 process.on('SIGINT', () => {
     in1.unexport();
     in2.unexport();
+    in3.unexport();
+    in4.unexport();
     process.exit();
 });
+
+
+
+
+// // Define GPIO pins for motor control
+// const { Gpio } = require('onoff');
+// const in1 = new Gpio(22, 'out'); // Use GPIO 22 for IN1 on L298N
+// const in2 = new Gpio(27, 'out'); // Use GPIO 27 for IN2 on L298N
+
+// // Variable to track motor state
+// let isMotorOn = false;
+
+// // Function to drive motor forward
+// function motorForward() {
+//     console.log('forward')
+//     in1.writeSync(1);
+//     in2.writeSync(0);
+// }
+
+// // Function to stop the motor
+// function motorStop() {
+//     in1.writeSync(0);
+//     in2.writeSync(0);
+// }
+
+// exports.testReq = async function (req, res) {
+//     try {
+//         // Toggle motor state
+//         isMotorOn = !isMotorOn;
+
+//         // Set the motor state based on the isMotorOn value
+//         if (isMotorOn) {
+//             motorForward();
+         
+//             console.log("motor on")
+//         } else {
+//             motorStop();
+//             console.log("motor off")
+//         }
+
+//         res.status(200).json({ status: "success", isMotorOn });
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({ status: "failure", message: "Failed to control GPIO pin" });
+//     }
+// };
+
+// // Cleanup GPIO on exit
+// process.on('SIGINT', () => {
+//     in1.unexport();
+//     in2.unexport();
+//     process.exit();
+// });
 
 
 
