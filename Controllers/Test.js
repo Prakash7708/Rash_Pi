13935d84c4ae743229c674c24c727a6e0ef31c22
Prakash <103762233+Prakash7708@users.noreply.github.com>
2025-01-24 21:48:04 +0530

    single moter

--- Controllers/Test.js
+++ Controllers/Test.js
@@ -25,170 +25,116 @@
 // };
 
 
-const { Gpio } = require('onoff');
-
-// Define GPIO pins for Motor 1
-const in1 = new Gpio(22, 'out'); // IN1 on L298N for Motor 1
-const in2 = new Gpio(27, 'out'); // IN2 on L298N for Motor 1
-//const ena = new Gpio(25, 'out'); // IN2 on L298N for Motor 1
-
-// Define GPIO pins for Motor 2
-const in3 = new Gpio(23, 'out'); // IN3 for Motor 2
-const in4 = new Gpio(24, 'out'); // IN4 for Motor 2
-
-
-let pwmInterval; // Declare pwmInterval globally for stopping later
-
-function setMotorSpeed(speed) {
-    const dutyCycle = Math.max(0, Math.min(speed, 100)); // Limit speed to 0-100%
-    const onTime = (dutyCycle / 100) * 10; // ON time in milliseconds
-    const offTime = 10 - onTime; // OFF time in milliseconds
-
-    // Clear any existing PWM intervals before starting a new one
-    if (pwmInterval) {
-        clearInterval(pwmInterval);
-    }
-
-    // Start the PWM interval
-    pwmInterval = setInterval(() => {
-        ena.writeSync(1); // Turn ENA on
-        setTimeout(() => {
-            ena.writeSync(0); // Turn ENA off
-        }, onTime); // Off after onTime
-    }, onTime + offTime); // Total cycle duration (10 ms)
-}
-
-// Function to stop the motor
-function stopMotor() {
-    if (pwmInterval) {
-        clearInterval(pwmInterval); // Stop the PWM interval
-        pwmInterval = null; // Reset the interval reference
-    }
-    ena.writeSync(0); // Ensure ENA is turned off
-    console.log("Motor stopped");
-}
-
-
-// Variables to track motor states
-let isMotor1On = false;
-let isMotor2On = false;
-
-// Function to drive Motor 1 forward
-function motor1Forward() {
-    console.log('Motor 1 forward');
-    in1.writeSync(1);
-    in2.writeSync(0);
-    //setMotorSpeed(30);
-}
-
-// Function to stop Motor 1
-function motor1Stop() {
-    in1.writeSync(0);
-    in2.writeSync(0);
-    //stopMotor()
-}
-
-// Function to drive Motor 2 forward
-function motor2Forward() {
-    console.log('Motor 2 forward');
-    in3.writeSync(1);
-    in4.writeSync(0);
-}
-
-// Function to stop Motor 2
-function motor2Stop() {
-    in3.writeSync(0);
-    in4.writeSync(0);
-}
-
-exports.testReq = async function (req, res) {
-    try {
-        const { motor } = req.query; // Check which motor to control from query parameter
-
-        if (motor === '1') {
-            // Toggle Motor 1 state
-            isMotor1On = !isMotor1On;
-            if (isMotor1On) {
-                motor1Forward();
-                console.log("Motor 1 on");
-            } else {
-                motor1Stop();
-                console.log("Motor 1 off");
-            }
-        } else if (motor === '2') {
-            // Toggle Motor 2 state
-            isMotor2On = !isMotor2On;
-            if (isMotor2On) {
-                motor2Forward();
-                console.log("Motor 2 on");
-            } else {
-                motor2Stop();
-                console.log("Motor 2 off");
-            }
-        }
-
-        // Send response with both motor statuses
-        res.status(200).json({
-            status: "success",
-            motorStatus: { isMotor1On, isMotor2On }
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ status: "failure", message: "Failed to control GPIO pins" });
-    }
-};
-
-// Cleanup GPIO on exit
-process.on('SIGINT', () => {
-    in1.unexport();
-    in2.unexport();
-    in3.unexport();
-    in4.unexport();
-    process.exit();
-});
-
-
-
-// // Define GPIO pins for motor control
 // const { Gpio } = require('onoff');
-// const in1 = new Gpio(22, 'out'); // Use GPIO 22 for IN1 on L298N
-// const in2 = new Gpio(27, 'out'); // Use GPIO 27 for IN2 on L298N
 
-// // Variable to track motor state
-// let isMotorOn = false;
+// // Define GPIO pins for Motor 1
+// const in1 = new Gpio(22, 'out'); // IN1 on L298N for Motor 1
+// const in2 = new Gpio(27, 'out'); // IN2 on L298N for Motor 1
+// //const ena = new Gpio(25, 'out'); // IN2 on L298N for Motor 1
 
-// // Function to drive motor forward
-// function motorForward() {
-//     console.log('forward')
-//     in1.writeSync(1);
-//     in2.writeSync(0);
+// // Define GPIO pins for Motor 2
+// const in3 = new Gpio(23, 'out'); // IN3 for Motor 2
+// const in4 = new Gpio(24, 'out'); // IN4 for Motor 2
+
+
+// let pwmInterval; // Declare pwmInterval globally for stopping later
+
+// function setMotorSpeed(speed) {
+//     const dutyCycle = Math.max(0, Math.min(speed, 100)); // Limit speed to 0-100%
+//     const onTime = (dutyCycle / 100) * 10; // ON time in milliseconds
+//     const offTime = 10 - onTime; // OFF time in milliseconds
+
+//     // Clear any existing PWM intervals before starting a new one
+//     if (pwmInterval) {
+//         clearInterval(pwmInterval);
+//     }
+
+//     // Start the PWM interval
+//     pwmInterval = setInterval(() => {
+//         ena.writeSync(1); // Turn ENA on
+//         setTimeout(() => {
+//             ena.writeSync(0); // Turn ENA off
+//         }, onTime); // Off after onTime
+//     }, onTime + offTime); // Total cycle duration (10 ms)
 // }
 
 // // Function to stop the motor
-// function motorStop() {
+// function stopMotor() {
+//     if (pwmInterval) {
+//         clearInterval(pwmInterval); // Stop the PWM interval
+//         pwmInterval = null; // Reset the interval reference
+//     }
+//     ena.writeSync(0); // Ensure ENA is turned off
+//     console.log("Motor stopped");
+// }
+
+
+// // Variables to track motor states
+// let isMotor1On = false;
+// let isMotor2On = false;
+
+// // Function to drive Motor 1 forward
+// function motor1Forward() {
+//     console.log('Motor 1 forward');
+//     in1.writeSync(1);
+//     in2.writeSync(0);
+//     //setMotorSpeed(30);
+// }
+
+// // Function to stop Motor 1
+// function motor1Stop() {
 //     in1.writeSync(0);
 //     in2.writeSync(0);
+//     //stopMotor()
+// }
+
+// // Function to drive Motor 2 forward
+// function motor2Forward() {
+//     console.log('Motor 2 forward');
+//     in3.writeSync(1);
+//     in4.writeSync(0);
+// }
+
+// // Function to stop Motor 2
+// function motor2Stop() {
+//     in3.writeSync(0);
+//     in4.writeSync(0);
 // }
 
 // exports.testReq = async function (req, res) {
 //     try {
-//         // Toggle motor state
-//         isMotorOn = !isMotorOn;
+//         const { motor } = req.query; // Check which motor to control from query parameter
 
-//         // Set the motor state based on the isMotorOn value
-//         if (isMotorOn) {
-//             motorForward();
-         
-//             console.log("motor on")
-//         } else {
-//             motorStop();
-//             console.log("motor off")
+//         if (motor === '1') {
+//             // Toggle Motor 1 state
+//             isMotor1On = !isMotor1On;
+//             if (isMotor1On) {
+//                 motor1Forward();
+//                 console.log("Motor 1 on");
+//             } else {
+//                 motor1Stop();
+//                 console.log("Motor 1 off");
+//             }
+//         } else if (motor === '2') {
+//             // Toggle Motor 2 state
+//             isMotor2On = !isMotor2On;
+//             if (isMotor2On) {
+//                 motor2Forward();
+//                 console.log("Motor 2 on");
+//             } else {
+//                 motor2Stop();
+//                 console.log("Motor 2 off");
+//             }
 //         }
 
-//         res.status(200).json({ status: "success", isMotorOn });
+//         // Send response with both motor statuses
+//         res.status(200).json({
+//             status: "success",
+//             motorStatus: { isMotor1On, isMotor2On }
+//         });
 //     } catch (error) {
 //         console.log(error);
-//         res.status(500).json({ status: "failure", message: "Failed to control GPIO pin" });
+//         res.status(500).json({ status: "failure", message: "Failed to control GPIO pins" });
 //     }
 // };
 
@@ -196,8 +142,62 @@ process.on('SIGINT', () => {
 // process.on('SIGINT', () => {
 //     in1.unexport();
 //     in2.unexport();
+//     in3.unexport();
+//     in4.unexport();
 //     process.exit();
 // });
+
+
+
+// Define GPIO pins for motor control
+const { Gpio } = require('onoff');
+const in1 = new Gpio(22, 'out'); // Use GPIO 22 for IN1 on L298N
+const in2 = new Gpio(27, 'out'); // Use GPIO 27 for IN2 on L298N
+
+// Variable to track motor state
+let isMotorOn = false;
+
+// Function to drive motor forward
+function motorForward() {
+    console.log('forward')
+    in1.writeSync(1);
+    in2.writeSync(0);
+}
+
+// Function to stop the motor
+function motorStop() {
+    in1.writeSync(0);
+    in2.writeSync(0);
+}
+
+exports.testReq = async function (req, res) {
+    try {
+        // Toggle motor state
+        isMotorOn = !isMotorOn;
+
+        // Set the motor state based on the isMotorOn value
+        if (isMotorOn) {
+            motorForward();
+         
+            console.log("motor on")
+        } else {
+            motorStop();
+            console.log("motor off")
+        }
+
+        res.status(200).json({ status: "success", isMotorOn });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ status: "failure", message: "Failed to control GPIO pin" });
+    }
+};
+
+// Cleanup GPIO on exit
+process.on('SIGINT', () => {
+    in1.unexport();
+    in2.unexport();
+    process.exit();
+});
 
 
 
